a7feba4ff91346358d875b63c8a3c8020dddbbdc
ben <ben@gmail.com>
2021-01-28 21:07:22 +0800

    teacher

---
+++ src/co/ben/bean/Teacher.java
@@ -0,0 +1,16 @@
+package co.ben.bean;
+
+public class Teacher {
+
+	private Integer age;
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+}
